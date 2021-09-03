dc641e1d50a3e633959cca034a1078ab08c5f346
Khalid Abuhakmeh <khalidabuhakmeh@gmail.com>
2021-09-03 14:23:27 -0400

    Include Anti-forgery Tokens as part of HTMX config

--- src/Htmx.TagHelpers/HtmxConfigMetaTagHelper.cs
+++ src/Htmx.TagHelpers/HtmxConfigMetaTagHelper.cs
@@ -1,7 +1,13 @@
+using System;
 using System.Text.Json;
+using System.Web;
+using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Htmx.TagHelpers
 {
@@ -19,7 +25,7 @@ namespace Htmx.TagHelpers
         /// </summary>
         [HtmlAttributeName("historyCacheSize")]
         public int? HistoryCacheSize { get; set; } = 10;
-        
+
         /// <summary>
         /// defaults to false, if set to true htmx will issue a full page refresh on history misses rather than use an AJAX request
         /// </summary>
@@ -31,76 +37,85 @@ namespace Htmx.TagHelpers
         /// </summary>
         [HtmlAttributeName("defaultSwapStyle")]
         public string? DefaultSwapStyle { get; set; }
-        
+
         /// <summary>
         /// defaults to 0
         /// </summary>
         [HtmlAttributeName("defaultSwapDelay")]
         public int? DefaultSwapDelay { get; set; }
-        
+
         /// <summary>
         /// defaults to 100
         /// </summary>
         [HtmlAttributeName("defaultSettleDelay")]
         public int? DefaultSettleDelay { get; set; }
-        
+
         /// <summary>
         /// defaults to true (determines if the indicator styles are loaded)
         /// </summary>
         [HtmlAttributeName("includeIndicatorStyles")]
         public bool? IncludeIndicatorStyles { get; set; }
-        
+
         /// <summary>
         /// defaults to htmx-indicator
         /// </summary>
         [HtmlAttributeName("indicatorClass")]
         public string? IndicatorClass { get; set; }
-        
+
         /// <summary>
         /// defaults to htmx-request
         /// </summary>
         [HtmlAttributeName("requestClass")]
         public string? RequestClass { get; set; }
-        
+
         /// <summary>
         /// defaults to htmx-settling
         /// </summary>
         [HtmlAttributeName("settlingClass")]
         public string? SettlingClass { get; set; }
-        
+
         /// <summary>
         /// defaults to htmx-swapping
         /// </summary>
         [HtmlAttributeName("swappingClass")]
         public string? SwappingClass { get; set; }
-        
+
         /// <summary>
         /// defaults to true
         /// </summary>
         [HtmlAttributeName("allowEval")]
         public bool? AllowEval { get; set; }
-        
+
         /// <summary>
         /// defaults to false, HTML template tags for parsing content from the server (not IE11 compatible!)
         /// </summary>
         [HtmlAttributeName("useTemplateFragments")]
         public bool? UseTemplateFragments { get; set; }
-        
+
         /// <summary>
         /// defaults to full-jitter
         /// </summary>
         [HtmlAttributeName("wsReconnectDelay")]
         public string? WsReconnectDelay { get; set; }
-        
+
         /// <summary>
         /// defaults to [disable-htmx], [data-disable-htmx], htmx will not process elements with this attribute on it or a parent
         /// </summary>
         [HtmlAttributeName("disableSelector")]
         public string? DisableSelector { get; set; }
 
+        [HtmlAttributeName("includeAspNetAntiforgeryToken")]
+        public bool IncludeAntiForgery { get; set; }
+
+        [ViewContext] 
+        public ViewContext ViewContext { get; set; } = null!;
+
+        private HttpContext HttpContext => ViewContext.HttpContext;
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
-            var config = JsonSerializer.Serialize(new {
+            var config = JsonSerializer.Serialize(new
+            {
                 AllowEval,
                 DisableSelector,
                 HistoryEnabled,
@@ -115,16 +130,17 @@ namespace Htmx.TagHelpers
                 IncludeIndicatorStyles,
                 UseTemplateFragments,
                 WsReconnectDelay,
-                RefreshOnHistoryMiss
+                RefreshOnHistoryMiss,
+                AntiForgery = TryGetAntiForgeryConfig()
             }, new JsonSerializerOptions
             {
                 WriteIndented = false,
                 IgnoreNullValues = true,
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase
             });
-            
+
             output.Attributes.RemoveAll("content");
-            
+
             // make sure to use single quotes
             // because JSON uses double quotes
             output.Attributes.Add(new TagHelperAttribute(
@@ -132,6 +148,38 @@ namespace Htmx.TagHelpers
                 new HtmlString(config),
                 HtmlAttributeValueStyle.SingleQuotes)
             );
+        }
+
+        private AntiForgeryConfig? TryGetAntiForgeryConfig()
+        {
+            if (!IncludeAntiForgery)
+                return null;
+
+            var antiforgery = HttpContext.RequestServices.GetService<IAntiforgery>();
+            if (antiforgery?.GetTokens(HttpContext) is { } tokens)
+            {
+                return new AntiForgeryConfig(tokens);
+            }
+
+            return null;
+        }
+
+        private class AntiForgeryConfig
+        {
+            public AntiForgeryConfig(AntiforgeryTokenSet? antiforgery)
+            {
+                if (antiforgery == null)
+                    throw new ArgumentNullException(nameof(antiforgery));
+
+                FormFieldName = antiforgery.FormFieldName;
+                HeaderName = antiforgery.HeaderName;
+                // important or token gets warped
+                RequestToken = HttpUtility.HtmlAttributeEncode(antiforgery.RequestToken);
+            }
+
+            public string FormFieldName { get; set; }
+            public string? HeaderName { get; set; }
+            public string RequestToken { get; set; }
         }
     }
 }
--- test/Htmx.Sample/wwwroot/js/site.js
+++ test/Htmx.Sample/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener("htmx:configRequest", (evt) => {
+    let httpVerb = evt.detail.verb.toUpperCase();
+    if (httpVerb === 'GET') return;
+    
+    let antiForgery = htmx.config.antiForgery;
+
+    if (antiForgery) {
+        
+        // already specified on form, short circuit
+        if (evt.detail.parameters[antiForgery.formFieldName])
+            return;
+        
+        if (antiForgery.headerName) {
+            evt.detail.headers[antiForgery.headerName]
+                = antiForgery.requestToken;
+        } else {
+             evt.detail.parameters[antiForgery.formFieldName]
+                = antiForgery.requestToken;
+        }
+    }
+});
